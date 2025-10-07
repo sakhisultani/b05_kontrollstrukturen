a52d57b7d8c9d9d2264dc7fe6d6c780b27034d63
vboxuser <sakhisultani2013@gmail.com>
2025-10-07 12:04:28 +0200

    add comments to the forschleife.java

--- b_for/ForSchleife.java
+++ b_for/ForSchleife.java
@@ -2,6 +2,19 @@ public class ForSchleife
 { 
     public static void main (String[] args)
     { 
+
+         // der kopf der for-schleife steuert die
+         // anzahl der wiederholungen.
+
+         // er hat deri abschnitte: 
+
+
+         // 1) deklaration der zeahlveriablen i  udna 
+         //  initialisierung mit dem Srtartwert 1.
+         // 2) bedingungh feur den endwert : i darf nur 
+         // klein oder gleich 20 sein (nicht groesser)
+         // 3) die schrittweite fuer das hochzeahlen der
+         // variablen i ist 1.
        for (int i = 1; i <= 20; i++)
        {
           System.out.println("Hallo " + i);
