2f87efd920d6ef9c7d9edcafe59bd5b305ff8109
vboxuser <sakhisultani2013@gmail.com>
2025-10-09 14:23:18 +0200

    beispiele if else schlechte code

---
+++ g_geschachteltes_if/ABCAnalyse2.java
@@ -0,0 +1,34 @@
+// schlechter code
+
+public class ABCAnalyse2
+{ 
+      public static void main (String[] args) 
+    {
+       char prioritaet = 'B';
+       if(prioritaet == 'A')
+       {
+          System.out.println("Hoch");
+       } 
+       else
+       {
+         
+       }
+
+          if(prioritaet == 'B')
+          {
+             System.out.println("Mittel"); 
+          }
+          else
+          {
+             if(prioritaet == 'c')
+             {
+                System.out.println("Niedrig"); 
+             }
+             else
+             {
+                System.out.println("Unzulaessing");
+             }
+         }
+        
+     }
+}
