542a67848d83f3b77e1904a02329fe0388e906fa
vboxuser <sakhisultani2013@gmail.com>
2025-10-10 09:17:27 +0200

    geschachteltes if else schlechter code

---
+++ g_geschachteltes_if/ABCAnalyse3.java
@@ -0,0 +1,27 @@
+
+// Schlechter code 
+
+public class ABCAnalyse3
+{ 
+    public static void main (String[] args) 
+    {
+       char prioritaet = 'X';
+       if(prioritaet == 'A')
+       {
+          System.out.println("Hoch");
+       } 
+      
+          if(prioritaet == 'B')
+          {
+             System.out.println("Mittel"); 
+          }
+          else
+          {
+            System.out.println("Niedrig");
+          }
+        if(prioritaet == 'X')
+        {
+           System.out.println("unzulaessiger wert");
+        }
+     }
+}
